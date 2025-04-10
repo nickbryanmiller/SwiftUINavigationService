66379338190ac4420396471fe360b7c9e23408da
Nicholas Miller <nickbryanmiller@gmail.com>
2025-04-09 21:22:04 -0700

    push

---
+++ NavigationDemo/NavigationDemo/Routing/HostingController.swift
@@ -0,0 +1,10 @@
+// HostingController.swift
+//
+// Created by Nicholas Miller on 4/9/25.
+// Copyright © 2025 Nicholas Miller. All rights reserved.
+
+import SwiftUI
+
+public final class HostingController<Content: View>: UIHostingController<Content> {
+  
+}
